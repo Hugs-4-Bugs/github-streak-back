aa65845670d6a986fdc26cfe4a35059b451e4bbc
Hugs-4-Bugs <prabhathkumar.cse@skit.org.in>
2022-01-01 15:12:03 +0530

    Random commit 1/5 for 2022-01-01

--- backfill.js
+++ backfill.js
@@ -4,35 +4,61 @@ const moment = require('moment');
 
 const git = simpleGit();
 
-async function backfill() {
+async function randomBackfill() {
     const startDate = moment('2022-01-01');
     const endDate = moment('2022-12-31');
-    const dateFormat = 'YYYY-MM-DDTHH:mm:ssZ';
 
     let currentDate = startDate.clone();
 
     while (currentDate.isSameOrBefore(endDate)) {
-        const dateString = currentDate.format(dateFormat);
-        console.log(`Committing for date: ${dateString}`);
+        // 1 se 25 ke beech random commits generate karo
+        // Agar tumhe max 25 se zyada chahiye toh 25 ki jagah koi aur number likho
+        const numCommits = Math.floor(Math.random() * 25) + 1; 
         
-        // 1. data.json me current date likho
-        jsonfile.writeFileSync('data.json', { date: dateString });
-        
-        // 2. Git ko batao ki commit kis date pe karna hai
-        process.env.GIT_AUTHOR_DATE = dateString;
-        process.env.GIT_COMMITTER_DATE = dateString;
-        
-        // 3. Commit karo
-        await git.add('./*');
-        await git.commit(`Streak backfill for ${dateString}`);
-        
-        // 4. Agli date pe jao
+        console.log(`Date: ${currentDate.format('YYYY-MM-DD')} | Total Commits: ${numCommits}`);
+
+        for (let i = 1; i <= numCommits; i++) {
+            // Random time generate karo (0 se 23 ghante, 0 se 59 minute, 0 se 59 second)
+            const randomHour = Math.floor(Math.random() * 24).toString().padStart(2, '0');
+            const randomMinute = Math.floor(Math.random() * 60).toString().padStart(2, '0');
+            const randomSecond = Math.floor(Math.random() * 60).toString().padStart(2, '0');
+            
+            // Final date string format: YYYY-MM-DDTHH:mm:ss+05:30
+            const dateString = `${currentDate.format('YYYY-MM-DD')}T${randomHour}:${randomMinute}:${randomSecond}+05:30`;
+
+            // data.json me change karo taaki Git commit kar sake
+            jsonfile.writeFileSync('data.json', { 
+                date: dateString,
+                commit_number: i,
+                total_commits_today: numCommits
+            });
+
+            // Git ko batao ki commit kis date aur time pe karna hai
+            process.env.GIT_AUTHOR_DATE = dateString;
+            process.env.GIT_COMMITTER_DATE = dateString;
+
+            // Commit karo
+            await git.add('./*');
+            await git.commit(`Random commit ${i}/${numCommits} for ${currentDate.format('YYYY-MM-DD')}`);
+        }
+
+        // Agli date pe jao
         currentDate.add(1, 'day');
     }
+
+    console.log('Sabhi commits ban gaye. Ab GitHub pe push kar rahe hain...');
     
-    console.log('Pushing all changes to GitHub...');
-    await git.push('origin', 'main');
-    console.log('Done! Sabhi commits push ho gaye.');
+    // Push karo (agar main branch nahi hai toh master try karega)
+    try {
+        await git.push('origin', 'main');
+        console.log('Successfully pushed to main branch!');
+    } catch (error) {
+        console.log('Main branch push fail hua, master try kar rahe hain...');
+        await git.push('origin', 'master');
+        console.log('Successfully pushed to master branch!');
+    }
+    
+    console.log('Done! GitHub profile check karo 1-2 minute baad.');
 }
 
-backfill().catch(err => console.error(err));
+randomBackfill().catch(err => console.error('Error aaya:', err));
